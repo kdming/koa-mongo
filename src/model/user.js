0926db07d61b22ceffae30d530171528bc831e2f
kdming <3012537771@qq.com>
2019-03-22 14:12:46 +0800

    保存用户时对密码进行加密处理

--- src/model/user.js
+++ src/model/user.js
@@ -1,7 +1,13 @@
+const crypto = require('crypto');
+
 module.exports = (Schema) => {
   const model = new Schema({
     name: String,
     pwd: String
+  });
+  model.pre('save', function (next) {
+    this.pwd = crypto.createHash('sha1').update(this.pwd).digest('hex');
+    next();
   });
   return {name: 'user', model};
 }
