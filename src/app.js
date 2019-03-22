9825b1669ce5fade447149f7771bcfb102cc3f0f
kdming <3012537771@qq.com>
2019-03-22 14:11:24 +0800

    完善项目启动

--- src/app.js
+++ src/app.js
@@ -1,17 +1,31 @@
+const convert = require('koa-convert');
 const koaBody = require('koa-body');
+const cors = require('koa-cors');
 const koa = require('koa');
 
-const router = require('./common/router/controller');
-const mongo = require('./common/mongo/mongo');
-const svc = require('../src/util/service');
+const loadRouter = require('./common/router/controller');
+const connect = require('./common/mongo/mongo');
+const token = require('./common/token/token');
 const config = require('./common/config');
 
 const app = new koa();
 app.use(koaBody());
 
-mongo.on('connected', () => {
-  app.use((ctx, next) => {svc(ctx, next)});
-  app.use(router.routes());
+app.use(convert(cors({
+  origin: '*',
+  credentials: true,
+  headers: ['Content-Type', 'accept', 'access-token', 'X-CLOUD-TOKEN', 'token', 'Access-Control-Allow-Credentials', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Headers'],
+  expose: ['Total', 'X-Response-Time', 'Content-Disposition']
+})));
+
+const {router, unroot} = loadRouter();
+app.use(unroot.routes())
+app.use(async (ctx, next) => {
+  await token.decryToken(ctx, next)
+});
+app.use(router.routes());
+
+connect().on('connected', () => {
   const appPort = config.parsed.appPort;
   app.listen(appPort);
   console.log('项目启动', appPort);
