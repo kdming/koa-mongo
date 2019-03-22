0de8c335f504127758298f9e3229ea861d1dd8c0
kdming <3012537771@qq.com>
2019-03-22 13:22:44 +0800

    更新工具类

--- src/util/service.js
+++ src/util/service.js
@@ -1,7 +1,17 @@
-const service = async (ctx, next) => {
-  ctx.returnBody = (success, data, message) => {
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+
+const service = {
+  returnBody: (success, data, message) => {
     return {success, data, message};
-  };
-  await next();
+  },
+  encryptStr: (str) => {
+    if (!str || typeof str !== 'string') return str;
+    const estr = crypto.createHash('sha1').update(str).digest('hex');
+    return estr;
+  },
+  objId: (id) => {
+    return mongoose.Types.ObjectId(id);
+  }
 }
 module.exports = service;
