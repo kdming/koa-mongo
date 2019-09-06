26358eaa5270cb5df9193e3179d7969794ef0f0d
kdming <3012537771@qq.com>
2019-09-06 10:45:26 +0800

    格式化文档，其实熟悉了不需要eslient

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "no-const-assign": 2,
-        "no-else-return": 2,
-        "no-undef": 2,
-        'no-console': 'off',
-        "no-extra-semi": 1,
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ]
-    }
+  "parser": "babel-eslint",
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    "no-const-assign": 2,
+    "no-else-return": 2,
+    "no-undef": 2,
+    'no-console': 'off',
+    "no-extra-semi": 1,
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ]
+  }
 };
